d4c978d53d76a2a0663203e7147c2a2846051877
Eric Gelphman <egelphman17@gmail.com>
2017-04-17 10:45:22 -0700

    Add files via upload
    Bug fixes

--- KrazyGlue.py
+++ KrazyGlue.py
@@ -1,5 +1,5 @@
 #Eric Gelphman
-#April 14, 2017 
+#April 17, 2017 
 #KrazyGlue Algorithm to investigate the mean curvature of spacetime near Black Holes
 
 #Perform a linear iterpolation if value of r is not found in numerical data
@@ -11,15 +11,18 @@ def linInterpolate(r, data, low, high):
 #values of r and f(r)
 #Note: In tuple structure, tuple[0] = r, tuple[1] = f(r)
 def binarySearch(r, data, low, high):
+    EPSILON = 0.01
     if low <= high:
         mid = int((low + high) / 2)
-        if abs(data[mid][0] - r) <= 0.01:         #r value is withtin epsilon
-            return data[mid]
-        elif data[mid][0] - r < -0.01:            #data[mid][0] < r 
-            binarySearch(r, data, mid + 1, high)
-        else:                                     #data[mid][0] > r
-            binarySearch(r, data, low, mid - 1)
+        if abs(data[mid][0] - r) <= EPSILON:         #r value is withtin epsilon
+            return data[mid][1]
+        else:
+            if data[mid][0] - r < 0:            #data[mid][0] < r 
+                return binarySearch(r, data, mid + 1, high)
+            else:                                     #data[mid][0] > r
+                return binarySearch(r, data, low, mid - 1)
     else:
+        print(str(data[mid][0]) + " " + str(data[mid][1]))
         return linInterpolate(r, data, low, high)
 
 #Function to calculate H using necessary parameters       
@@ -76,7 +79,7 @@ def readFromFile():
         for line in lines:
             split = line.partition("_")
             try:
-                f_data.append((float(split[0]), float(split[2])))
+                f_data.append(((round(float(split[0]), 4)), round(float(split[2]), 5)))
             except ValueError:
                 break
     lines.close()        
@@ -92,25 +95,26 @@ def checkBoundaryCondition(r, f, fp, fpp, EPSILON):
         else:
             print("Boundary Conditions Met!")    
         
-    
 def main():     
     STEP_SIZE = 0.1
     f_data = readFromFile()
     file2 = open("output3.txt", 'r+')
     #Traversal
     r = 2.0
-    f_prev = 0.0
+    f_prev = -10.0
+    f = f_prev
     fprime_prev = 0.0
-    r_prev = 0.0
-    while r >= -9992.0:
+    r_prev = 2.1
+    while r >= -9999.7:
         #f_data must be sorted in increasing order
-        f = binarySearch(r, f_data)
+        f = binarySearch(r, f_data, 0, len(f_data) - 1)
         fp = f - f_prev / (r - r_prev)
         fpp = fp - fprime_prev / (r - r_prev)
         #Check boundary conditions around r = 1.5
         #if (r >= 1.4 and r <= 1.6):
             #checkBoundaryCondition(r, f, fp, fpp, EPSILON)  
-        output = calculateH(r, f, fp, fpp)    
+        output = calculateH(r, f, fp, fpp)   
+        print(output)
         if (output != "not real" and output != "undefined"):    
             H = output
             line = "r: " + str(r) + " f: " + str(f) + " fp: " + str(fp) + " fpp: " + str(fpp) + " H: " + str(H) + "\n"
@@ -124,16 +128,3 @@ def main():
         
 if __name__ == "__main__":
     main()        
-
-
-    
-
-        
-    
-
-
-
-    
-        
-    
-    
--- fGenerator.py
+++ fGenerator.py
@@ -1,5 +1,5 @@
 #Eric Gelphman
-#April 14, 2017 
+#April 17, 2017 
 #KrazyGlue Algorithm to investigate the mean curvature of spacetime near Black Holes
 #fGenerator script to generate values of f
 from sympy import Symbol, sympify, simplify
@@ -27,18 +27,20 @@ class FunctionStruct:
 
 def main():        
     #################################Start of Actual Script########################        
-    wfileN = input("Enter name of file that will store the data: ")
+    wfileN1 = "test3.txt"
+    wfileN2 = "test3_1.txt"
+    wfileN3 = "test3_2.txt"
     h = 0.1
     expr_f = sympify("r - 4/3 - 1/(3*r) + 4/(9*r**2)", evaluate=False)
     obj = FunctionStruct(expr_f, h, -10000, -10.2)
-    obj.generateFile(wfileN)
+    obj.generateFile(wfileN1)
     expr_glue = sympify("342.850*r**3 + 10282.990*r**2 + 102975.017*r + 342491.827", evaluate=False)
     obj = FunctionStruct(expr_glue, h, -10.1, -9.9)
-    obj.generateFile(wfileN)
+    obj.generateFile(wfileN2)
     r = -9.8
-    fileW = open(wfileN, 'r+')
+    fileW = open(wfileN3, 'r+')
     while r <= 2.0:
-        fileW.write(str(r) + "_" + "10.0" + "\n")
+        fileW.write(str(r) + "_" + "-10.0" + "\n")
         r += h
 
 if __name__ == "__main__":
