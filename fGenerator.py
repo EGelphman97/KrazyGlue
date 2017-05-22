76ba9c61708191642aa8e28c50a65f5572cb64bf
Eric Gelphman <egelphman17@gmail.com>
2017-05-21 17:18:57 -0700

    Add files via upload
    Fixed traversal order

--- fGenerator.py
+++ fGenerator.py
@@ -6,35 +6,35 @@ import math
 import matplotlib.pyplot as plt
 
 def parametricGenerator(filename):
-    s = 0.0 
-    r = 0.0
+    r = -10000.0
     t = 0.0
     r_vals = []
     t_vals = []
-    S_MAX = 10 - 2*(2/9)**(1/3)
     file1 = open(filename, 'r+')
     #Starting traversal from left side
-    while s <= S_MAX:
+    while r <= -3.50:
+        file1.write(str(r) + "_-5.0\n")
+        r_vals.append(r)
+        t_vals.append(-5.0)
+        r += 0.01
+    #glue region
+    while r < -3.2798:
+        #g(r) = -33.072r^3 - 334.005r^2 - 1122.631r - 1260.619
+        t = -33.072*r**3 - 334.005*r**2 - 1122.631*r - 1260.619
+        r_vals.append(r)
+        t_vals.append(t)
+        file1.write(str(r) + "_" + str(t) + "\n")  
+        r += 0.01  
+    S_START = 8.728016
+    s = S_START    
+    while r <= 2.0:
         rho = (10 - s) * (9/2)**(1/3)
         t = 2*math.sqrt(2*rho) - 4*math.atanh(math.sqrt(2/rho))
         r = (10 - s)**(3/2) + t
         r_vals.append(r)
         t_vals.append(t)
         file1.write(str(r) + "_" + str(t) + "\n")
-        s += 0.01
-    #glue region
-    while r < -3.0:
-        #g(r) = -141.949r^3 - 1338.955r^2 - 4201.114r - 4388.363 
-        t = -141.949*r**3 - 1338.955*r**2 - 4201.114*r - 4388.363
-        r += 0.01
-        r_vals.append(r)
-        t_vals.append(t)
-        file1.write(str(r) + "_" + str(t) + "\n")
-    while r <= 2.0:
-        file1.write(str(r) + "_-3.0\n")
-        r_vals.append(r)
-        t_vals.append(3.0)
-        r += 0.01
+        s -= 0.001  
     file1.close()
     return (r_vals, t_vals)     
         
