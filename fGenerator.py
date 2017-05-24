7d46df33648e77380389894714b4a323d2e2b041
Eric Gelphman <egelphman17@gmail.com>
2017-05-24 11:44:26 -0700

    Add files via upload
    Fixed gluing for f (again). Modified main traversal loop to calculate f' and f'' in addition to r and t = f(r).

--- fGenerator.py
+++ fGenerator.py
@@ -8,35 +8,42 @@ import matplotlib.pyplot as plt
 def parametricGenerator(filename):
     r = -10000.0
     t = 0.0
-    r_vals = []
-    t_vals = []
-    file1 = open(filename, 'r+')
-    #Starting traversal from left side
-    while r <= -3.50:
-        file1.write(str(r) + "_-5.0\n")
-        r_vals.append(r)
-        t_vals.append(-5.0)
-        r += 0.01
-    #glue region
-    while r < -3.2798:
-        #g(r) = -38.781r^3 - 392.681r^2 - 1323.548r - 1489.831
-        t = -38.781*r**3 - 392.681*r**2 - 1323.548*r - 1489.831
-        r_vals.append(r)
-        t_vals.append(t)
-        file1.write(str(r) + "_" + str(t) + "\n")  
-        r += 0.01  
     S_START = 8.728016
-    s = S_START    
+    s = S_START  
+    #data is a 4-tuple that holds the 4 essential values: r, t = f(r), f', and f''
+    data = ([],[],[],[])
+    file1 = open(filename, 'r+')
     while r <= 2.0:
-        rho = (10 - s) * (9/2)**(1/3)
-        t = 2*math.sqrt(2*rho) - 4*math.atanh(math.sqrt(2/rho))
-        r = (10 - s)**(3/2) + t
-        r_vals.append(r)
-        t_vals.append(t)
-        file1.write(str(r) + "_" + str(t) + "\n")
-        s -= 0.001  
+        #Starting traversal from left side
+        if r <= -3.50:
+            data[0].append(r)
+            data[1].append(-4.4)
+            fp = 0.0
+            fpp = 0.0
+            data[2].append(fp)
+            data[3].append(fpp)
+            r += 0.01
+        #glue region
+        elif r > 3.50 and r < -3.2798:
+            #g(r) = -18.876r^3 - 179.157r^2 - 565.283r - 597.495
+            t = -18.876*r**3 - 179.157*r**2 - 565.283*r - 597.495
+            data[0].append(r)
+            data[1].append(t)
+            fp = -56.629*r**2 - 358.314*r - 565.283
+            fpp = -113.257*r -358.314
+            data[2].append(fp)
+            data[3].append(fpp)
+            r += 0.01  
+        else:
+            rho = (10 - s) * (9/2)**(1/3)
+            t = 2*math.sqrt(2*rho) - 4*math.atanh(math.sqrt(2/rho))
+            r = (10 - s)**(3/2) + t
+            data[0].append(r)
+            data[1].append(t)
+            s -= 0.001 
+        file1.write("r: " + str(r) + " t: " + str(t) + " f': " + str(fp) + " f'' " + str(fpp) + "\n")         
     file1.close()
-    return (r_vals, t_vals)     
+    return data     
         
 def graphf(r_vals, t_vals):
     plt.plot(r_vals, t_vals)
