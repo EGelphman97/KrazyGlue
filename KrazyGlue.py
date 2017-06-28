b6db2530468a52f52788338894ae759381287952
Eric Gelphman <egelphman17@gmail.com>
2017-06-28 16:52:44 -0700

    Add files via upload
    Fixed gluing for M and t0 by expanding boundary conditions. Also calculated the general forms of the partial derivatives of X and Y. Mathematica is needed to check everything so we can calculate H.

--- KrazyGlue.py
+++ KrazyGlue.py
@@ -1,10 +1,11 @@
 #Eric Gelphman
-#May 8, 2017 
+#June 28, 2017 
 #KrazyGlue Algorithm to investigate the mean curvature of spacetime near Black Holes
 
 import matplotlib.pyplot as plt
 import fGenerator
 
+"""
 #Perform a linear iterpolation if value of r is not found in numerical data
 def linInterpolate(r, data, low, high):
     m = (data[high][1] - data[low][1]) / (data[high][0] - data[low][0]) #caluclate slope
@@ -29,8 +30,12 @@ def binarySearch(r, step_size, data, low, high):
     else:
         print("interpolation needed r: " + str(r) + "low: " + str(low) + "high: " + str(high))
         return linInterpolate(r, data, low, high)
+"""        
 
-#Function to calculate H using necessary parameters       
+#Function to calculate H given r, t = f(r), f'(r), and f''(r)
+#Returns value of H for the 4-tuple
+#Note: This value represents the solution to the 2nd Order Nonlinear ODE that
+#represents H at the r-value given in the parameters  
 def calculateH(r, t, fp, fpp): 
     X = 0.0 
     Y = 0.0
@@ -39,22 +44,24 @@ def calculateH(r, t, fp, fpp):
     dY_dr = 0.0
     dY_dt = 0.0
     #Calculate X, Y, and their partial derivatives
-    #Note: Need to account for where divide by 0 errors occur, as well as
-    #when a negative value is under the square root 
-    if r <= 0.9:                       #Schwarzchild Space
+    #Note: Need to account for where divide by 0 errors occur, as well as when a negative value is under the square root 
+    #Schwarzchild Space: M(r) = 1, t0(r) = r
+    if r <= 0.9:                       
         X = 2 / ((6 * (r - t)) ** (1/3))
         Y = 1.65096 * ((r - t) ** (2/3)) 
         dX_dr = -0.36688 / ((r - t) ** (4/3))
         dX_dt = -dX_dr
         dY_dr = 1.10064 / ((r - t) ** (1/3))
-        dY_dt = -dY_dr  
-    elif r >= 1.1:                    #Friedman Space
+        dY_dt = -dY_dr 
+    #Friedman Space: M(r) = r^3, t0(r) = 1    
+    elif r >= 1.1:                    
         X = 1.65096 * ((1 - t) ** (2/3))
         Y = 1.65096 * (((r ** 3) * ((1 - t) ** 2)) ** (1/3))
         #dX_dr = 0.0
         dX_dt = (-1.10064 * (r ** 2)) / ((-((r ** 6)*(t - 1))) ** (1/3))
         dY_dr = (1.65096 * (r ** 2) * ((1 - t) ** 2)) / (((r ** 3) * ((1 - t) ** 2)) ** (2/3))
         dY_dt = (-1.10064 * (r ** 3) * (1 - t)) / (((r ** 3) * ((1 - t) ** 2)) ** (2/3))
+    #Glue Region: M(r) = 4.25r^3 - 7.35r^2 + 3.6r + 0.648, t0(r) = -1.25r^2 + 3r - 0.8   
     else:
         X_num = (2.4825 * r) - (1.655 * t) + 0.2553
         X_denom = (8.2171 * r ** 3) + (2.5346 * r ** 2) - (16.4342 * r ** 2 * t) - (3.6559 * r) + (9.7215 * r * t) - 1.4377 * t + 0.6469
@@ -76,79 +83,37 @@ def calculateH(r, t, fp, fpp):
     else:
         return H_Num / H_Denom
 
-"""        
-#Function to read values of r, f(r) from file and store them as a tuple <r, f(r)>
-#The tuples are then stored in ascending r-order in a list, which is returned   
-def readFromFile():
-    with open("test3.txt", "r") as lines:
-        f_data = []
-        for line in lines:
-            split = line.partition("_")
-            try:
-                f_data.append(((round(float(split[0]), 6)), round(float(split[2]), 7)))
-            except ValueError:
-                break
-    lines.close()        
-    return f_data
-"""    
-    
-#Function to check boundary conditions around r = 1.5   
-#f'(r) and f''(r) need to be 0 at r = 1.5 or about there 
-def checkBoundaryCondition(r, f, fp, fpp, EPSILON):
-    if r >= 1.4 and r <= 1.6:
-        if(abs(f - r + 4/3) >= EPSILON or abs(fp - 1) >= EPSILON  or abs(fpp) >= EPSILON):
-            print("Boundary conditions not met!")
-            print("r = " + str(r) + " f(r) = " + str(f) + " f'(r) = " + str(fp) + " f''(r) = " + str(fpp))    
-        else:
-            print("Boundary Conditions Met!")  
 
-def graphH(r_vals, f_vals, H_vals):
-    #plt.plot(r_vals, f_vals)
-    #plt.xlabel("r")
-    #plt.ylabel("f(r)")
-    #plt.show()
+#Function to graph r vs. H            
+def graphH(r_vals, H_vals):
     plt.plot(r_vals, H_vals)
     plt.xlabel("r")
     plt.ylabel("H(r)")
     plt.show() 
         
 def main():    
-    fileni = "test4.txt"
+    fileni = "test5.txt"
     data = fGenerator.parametricGeneratorparametricGenerator(fileni)
-    fGenerator.graphf(data[0], data[1])
-    STEP_SIZE = 0.01
-    fileno = open("output3.txt", 'r+')
+    fileno = open("output5.txt", 'r+')
+    #Lists to store data to graph H
     r_vals = []
-    f_vals = []
     H_vals = []
-    #Traversal
-    r = data[0][0]
-    f = data[1][0]
-    while r <= 2.0:
-         #f_data must be sorted in increasing order
-        f = binarySearch(r, STEP_SIZE, data, 0, len(data) - 1)
-        if r <= -3.50:      #Not in the glue region
-            fp = 0.0
-            fpp = 0.0
-        elif r >= -3.2798:
-            #TODO
-            #Derivatives where f is parametric
-        else:
-            fp = -116.343*r**2 - 785.362*r - 1323.548
-            fpp = -232.686*r - 785.362
-        #Check boundary conditions around r = 1.5
-        #if (r >= 1.4 and r <= 1.6):
-            #checkBoundaryCondition(r, f, fp, fpp, EPSILON)  
-        output = calculateH(r, f, fp, fpp)
+    #Traversal to iterate through data containing necessary information stored in 4-tuples
+    #Step size is 1 index in list
+    for i in data:
+        r = data[i][0]
+        f = data[i][1]
+        fp = data[i][2]
+        fpp = data[i][3]
+        output = calculateH(r, f, fp, fp)
         if (output != "not real" and output != "undefined"):    
             H = output
             line = "r: " + str(r) + " f: " + str(f) + " fp: " + str(fp) + " fpp: " + str(fpp) + " H: " + str(H) + "\n"
             fileno.write(line)
             r_vals.append(r)
-            f_vals.append(f)
             H_vals.append(H)
-        r += STEP_SIZE
-    graphH(r_vals, f_vals, H_vals)        
+    fileno.close()        
+    graphH(r_vals, H_vals)        
         
         
 if __name__ == "__main__":
