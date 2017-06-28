8b16ae08f5dedd8c90951922d16b9873bfb2eb9c
Eric Gelphman <egelphman17@gmail.com>
2017-06-27 20:14:44 -0700

    Add files via upload
    General Big Fixes
    Calculation of r, f(r), f'(r), and f''(r) for all regions of interest (including glue region)
    Generation of graph of f(r)

--- fGenerator.py
+++ fGenerator.py
@@ -1,57 +1,78 @@
 #Eric Gelphman
-#May 26, 2017 
+#June 27, 2017 
 #KrazyGlue Algorithm to investigate the mean curvature of spacetime near Black Holes
 #fGenerator script to generate values of f
 import math 
 import matplotlib.pyplot as plt
 
+#Function to generate values for r, f(r), f'(r), and f''(r)
+#Filename is name of file where data will be stored
+#Returns list of 4-tuples with values organized in this format [r, f(r), f'(r), f''(r)]
 def parametricGenerator(filename):
     r = -10000.0
     t = 0.0
-    s = 0.0 
-    #data is a 4-tuple that holds the 4 essential values: r, t = f(r), f', and f''
+    y = 2520.0
+    #data is a list of 4-tuples that holds the 4 essential values: r, t = f(r), f', and f''
     data = []
-    file1 = open(filename, 'r+')
+    file1 = open(filename, 'r+') #open file
+    #Traversal starts from left side (r -> negative infinity)
     while r <= 2.0:
-        #Starting traversal from left side
-        if r <= -3.2798:
-            rho = (10 - s) * (9/2)**(1/3)
-            t = 2*math.sqrt(2*rho) - 4*math.atanh(math.sqrt(2/rho))
-            r = (10 - s)**(3/2) + t
-            rho3 = rho**3 - 1
-            fp = -5.45137/((2*rho)**(3/2)) - 3.85468/(rho**(3/2)*rho3) - 23.1281*rho**(3/2)/(rho3**2) + 3/(4*(10-s)**(1/2))
-            fpp = -27.0001/((2*rho)**(5/2)) - 9.54588/(rho**(5/2)*rho3) + 38.1835*(rho**(1/2))/(rho3**2) - 229.101*rho**(7/2)/(rho3**3) + 3/(8*(10-s)**(3/2))
-            data.append((r, t, fp, fpp))
-            s += 0.001 
+        #f(r) defined parametrically for r <= -1
+        #where r = -1.0, y = 1.5451
+        if r <= -1.20:
+            a = math.tanh(y)
+            t = 4*a - 4*y
+            r = (4.0 / (3.0*(a**(3/2)))) + t
+            #For large values of y, cosh(y) -> infinity, so sech(y)-> 0 (b terms) for large y
+            #Case for when cosh(y) is not large
+            try:
+                b = 1 / ((math.cosh(y))**2)
+                fp = (2*a**(9/2)) / (b + 2.0*a**(9/2))
+                fpp = ((7.0 + 2.0*math.cosh(2.0*y))*(b**2)*(a**6)) / (((2.0*b + 4.0*a**(9/2)))**3)
+            #Case for when cosh(y) is large    
+            except OverflowError:
+                fp = 1.0
+                fpp = 0.0
+            y -= 0.01
         #glue region
-        elif r > -3.2798 and r < -3.0:
-            #g(r) = -18.876r^3 - 179.157r^2 - 565.283r - 597.495
-            t = -18.876*r**3 - 179.157*r**2 - 565.283*r - 597.495
-            fp = -56.629*r**2 - 358.314*r - 565.283
-            fpp = -113.257*r -358.314
-            data.append((r, t, fp, fpp))
+        elif r > -1.20 and r < -0.80:
+            #g(r) = -0.8494r^3 - 3.6751r^2 - 4.2494r - 3.9823
+            t = -0.8494*r**3 - 3.6751*r**2 - 4.2494*r - 3.9823
+            fp = -2.5481*r**2 - 7.3503*r - 4.2494
+            fpp = -5.0963*r - 7.3503
             r += 0.01  
+        #Region where f(r) is constant    
         else:
-            t = -4.4
+            t = -2.50
             fp = 0.0
             fpp = 0.0
-            data.append((r, t, fp, fpp))
             r += 0.01
+        data.append((r, t, fp, fpp))    #Append to list
+        #write to file
         file1.write("r: " + str(r) + " t: " + str(t) + " f': " + str(fp) + " f'' " + str(fpp) + "\n")         
-    file1.close()
-    return data     
-        
+    file1.close() #close file
+    return data  #return list   
+     
+#Function to graph f(r)    
 def graphf(r_vals, t_vals):
     plt.plot(r_vals, t_vals)
     plt.xlabel("r")
     plt.ylabel("t")
     plt.show()    
 
-def main():        
-    #################################Start of Actual Script######################## 
-    filen = "test4.txt"
+def main():    
+    filen = "test5.txt"
     data = parametricGenerator(filen)
-    #graphf(data[0], data[1])
+    #Graph r vs. t
+    i = 0
+    r_vals = []
+    t_vals = []
+    while i < len(data):
+        r_vals.append(data[i][0])
+        t_vals.append(data[i][1])
+        i += 1
+    graphf(r_vals, t_vals)
+    
 
 if __name__ == "__main__":
     main()        
