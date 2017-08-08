0534d1bedc9ffacbd8e51d275fca2e776eaa3cfa
Eric Gelphman <egelphman17@gmail.com>
2017-08-07 17:31:56 -0700

    Add files via upload
    Changed fGenerator so it generates values of fear = f - r + 4/3 instead of f. Again, f and fbar are defined parametrically for large negative values of r, though the parametrization is different this time. Differentiation was changed to be done using finite difference methods. Since fbar' ~ 1 and fbar'' ~ 0 for r << 0, this should result in far faster runtimes as well as fewer numerical errors for a minimal loss of precision.

--- fGenerator.py
+++ fGenerator.py
@@ -1,84 +1,79 @@
 #Eric Gelphman
-#June 27, 2017 
+#June 27, 2017
 #KrazyGlue Algorithm to investigate the mean curvature of spacetime near Black Holes
 #fGenerator script to generate values of f
-import math 
+import math
 import matplotlib.pyplot as plt
 
-#Function to generate values for r, f(r), f'(r), and f''(r)
-#Filename is name of file where data will be stored
+#Function to generate values for r, fbar = f - r - 4/3, fbar', and fbar'' in the parametric region
+#r_init, f_bar_init, and fp_init are initial values for r, fbar, fbar', and fbar'' respectively
 #Returns list of 4-tuples with values organized in this format [r, f(r), f'(r), f''(r)]
-def parametricGenerator(filename):
-    r = -10000.0
-    t = 0.0
-    y = 2520.0
-    #data is a list of 4-tuples that holds the 4 essential values: r, t = f(r), f', and f''
+def parametricGenerator():
+    r = -10002.0
+    f_bar = fp = fpp = 0.0
+    x = 0.5
+    idx = 0
+    #data is a list of 4-tuples that holds the 4 essential values: r, fbar = f - r + 4/3, fbar', and fbar''
     data = []
-    file1 = open(filename, 'r+') #open file
     #Traversal starts from left side (r -> negative infinity)
-    while r <= 2.0:
-        #f(r) defined parametrically for r <= -1
-        #where r = -1.0, y = 1.5451
-        if r <= -1.20:
-            a = math.tanh(y)
-            t = 4*a - 4*y
-            r = (4.0 / (3.0*(a**(3/2)))) + t
-            #For large values of y, cosh(y) -> infinity, so sech(y)-> 0 (b terms) for large y
-            #Case for when cosh(y) is not large
-            try:
-                b = 1 / ((math.cosh(y))**2)
-                fp = (2*a**(9/2)) / (b + 2.0*a**(9/2))
-                fpp = ((7.0 + 2.0*math.cosh(2.0*y))*(b**2)*(a**6)) / (((2.0*b + 4.0*a**(9/2)))**3)
-            #Case for when cosh(y) is large    
-            except OverflowError:
-                fp = 1.0
-                fpp = 0.0
-            y -= 0.01
-        #glue region
-        elif r > -1.20 and r < -0.80:
-            #g(r) = -0.8494r^3 - 3.6751r^2 - 4.2494r - 3.9823
-            t = -0.8494*r**3 - 3.6751*r**2 - 4.2494*r - 3.9823
-            fp = -2.5481*r**2 - 7.3503*r - 4.2494
-            fpp = -5.0963*r - 7.3503
-            r += 0.01  
-        #Region where f(r) is constant    
-        else:
-            t = -2.50
-            fp = 0.0
-            fpp = 0.0
-            r += 0.01
-        data.append((r, t, fp, fpp))    #Append to list
-        #write to file
-        file1.write("r: " + str(r) + " t: " + str(t) + " f': " + str(fp) + " f'' " + str(fpp) + "\n")         
-    file1.close() #close file
-    return data  #return list   
-     
-#Function to graph f(r)    
+    while r <= -2.0:
+        a = 1 - x
+        r = 4/(3*a) + 4*a - 4*math.arctanh(a)
+        #will need to do taylor series expansion of fbar for large negative values of r
+        if r < -60.0:
+            f_bar = -2*x + (5/2)*x**2 - (35/12)*x**3
+        f_bar = 4/3 - 4/(3*a**(3/2))            #fbar = f - r + 4/3
+        data.append((r, f_bar, 0.0, 0.0)) #Append to list
+        if idx >= 2:
+            #Assign values to index idx - 1 in list
+            #Finite difference differentiation
+            val_forward = data[idx+1]     #tuple at index idx + 1
+            val_backward = data[idx-1]    #tuple at index 
+            fp = (val_forward[1] - val_backward[1]) / (val_forward[0] - val_backward[0])   #fbar'
+            fpp = (val_forward[1] - 2*f_bar + val_backward[1]) / (val_forward[0] - val_backward[0])**2        #fbar''
+            data[idx-1][2] = fp
+            data[idx-1][3] = fpp
+        x += 0.01
+        idx += 1    
+    return data  #return list
+
+#Function to generate values of r, fbar, fbar', and fbar'' for the glue and constant regions
+#gr_lower is lower bound of glue region, gr_upper is upper bound for glue region, r_upper is highest r value in traversal
+#Returns list of 4-tuples with values organized in this format [r, f(r), f'(r), f''(r)]
+def otherGen(gr_lower, gr_upper, r_upper):
+    data = []
+    return data
+    
+def writeToFile(filename, data):
+    file1 = open(filename, 'r+') #open file
+    for i in data:
+         #write to file
+        file1.write("r: " + str(i[0]) + " f_bar: " + str(i[1]) + " f_bar': " + str(i[2]) + " f_bar'': " + str(i[3]) + "\n")
+    file1.close()#close file
+
+#Function to graph fbar(r)
 def graphf(r_vals, t_vals):
     plt.plot(r_vals, t_vals)
     plt.xlabel("r")
-    plt.ylabel("t")
-    plt.show()    
+    plt.ylabel("fbar(r)")
+    plt.show()
 
-def main():    
-    filen = "test5.txt"
-    data = parametricGenerator(filen)
+def main():
+    fData = parametricGenerator()
+    fData.extend(otherGen(-1.0, -0.8, 2.0))
     #Graph r vs. t
-    i = 0
     r_vals = []
-    t_vals = []
-    while i < len(data):
-        r_vals.append(data[i][0])
-        t_vals.append(data[i][1])
-        i += 1
-    graphf(r_vals, t_vals)
-    
+    fbar_vals = []
+    for i in fData:
+        r_vals.append(i[0])
+        fbar_vals.append(i[1])
+    graphf(r_vals, fbar_vals)
+    #Ask user if they want file of numerical data in addition to graph
+    print("Enter 1 to generate file of numerical data, enter 0 to end\n")
+    response = input()
+    if response == 1:
+        writeToFile("test5.txt", fData)
+
 
 if __name__ == "__main__":
-    main()        
-   
-
-
-     
-            
-            
+    main()
